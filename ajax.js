873b0629dea8ae999eb26bd7e815e1ebf33b6418
Rashin Sergey <serhanters@yandex.ru>
2016-05-07 12:03:28 +0400

    Update ajax.js
    поправки в работе data:params

--- ajax.js
+++ ajax.js
@@ -1,130 +1,158 @@
-$AJAX = function (obj) {
-    var i,
-        res,
-        url,
-        xhr,
-        status,
-        statusText,
-        callback,
-        responses = {},
-        availableType       = ['GET', 'POST', 'PUT'],
-        availableDataType   = ['json', 'text'],
-        headers             = {},
-        xmlhttpobj          = XMLHttpRequest,
-        Msxml2              = 'Msxml2.XMLHTTP',
-        args = {
-            url:            obj.url             || false,
-            async:          obj.async           || false,
-            data:           obj.data            || null,
-            crossDomain:    obj.crossDomain     || false,
-            complete:       obj.result          || function(){},
-            timeout:        obj.timeout         || 10000,
-            type:           ( availableType.indexOf(obj.type) !== -1 ? obj.type : null )                || "GET",
-            dataType:       ( availableDataType.indexOf(obj.dataType) !== -1 ? obj.dataType : null )    || "json"
-        };
+    /**
+     *  SUPPOORT
+     *
+     *   CORS is supported in the following browsers:
+     *
+     *   Chrome 3+
+     *   Firefox 3.5+
+     *   Opera 12+
+     *   Safari 4+
+     *   Internet Explorer 8+
+     *
+     * @author Sergey Rashin
+     * @link https://github.com/serhanters/sAJAX
+     */
+    var sAJAX = function (obj) {
+        var i,
+            res,
+            url,
+            xhr,
+            status,
+            statusText,
+            callback,
+            responses = {},
+            und       = 'undefined',
+            availableType       = ['GET', 'POST', 'PUT'],
+            availableDataType   = ['json', 'text'],
+            headers             = {},
+            xmlhttpobj          = XMLHttpRequest,
+            Msxml2              = 'Msxml2.XMLHTTP',
+            args = {
+                url:            obj.url             || false,
+                async:          obj.async           || false,
+                data:           obj.data            || null,
+                crossDomain:    obj.crossDomain     || false,
+                complete:       obj.result          || function(){},
+                timeout:        obj.timeout         || 10000,
+                type:           ( availableType.indexOf(obj.type) !== -1 ? obj.type : null )                || "GET",
+                dataType:       ( availableDataType.indexOf(obj.dataType) !== -1 ? obj.dataType : null )    || "json"
+            };
 
-    if (typeof args.url === und || args.url === false) {
-        return;
-    }
-
-    if (typeof xmlhttpobj == und){
-        xmlhttpobj = function () {
-            try {
-                var activex_obj = ActiveXObject;
-            } catch ( e ) {}
-            try {
-                return new activex_obj( Msxml2+'.6.0' );
-            } catch ( e ) {}
-            try {
-                return new activex_obj( Msxml2+'.3.0' );
-            } catch ( e ) {}
-            try {
-                return new activex_obj( Msxml2 );
-            } catch ( e ) {}
-            throw new Error( 'This browser does not support XMLHttpRequest.' );
-        };
-    }
-    xhr = new (("onload" in new xmlhttpobj()) ? xmlhttpobj : XDomainRequest)();
-
-    url = args.url;
-    if (args.type === 'GET' && args.data && args.data.length>0) {
-        url = args.url + '?' +args.data.toString();
-    }
-    xhr.open( args.type, url, args.async );
-
-    if ( !args.crossDomain ) {
-        headers["X-Requested-With"] = "XMLHttpRequest";
-    }
-
-    for ( i in headers ) { // Support: IE<9
-        if ( headers.hasOwnProperty(i) && typeof headers[ i ] !== 'undefined' ) {
-            xhr.setRequestHeader( i, headers[ i ] + "" );
+        if (cbh.helpers.empty(args.url)) {
+            return;
         }
-    }
 
-    if (args.data && args.data.toString().length>0) {
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        args.data = args.data.toString();
-    }
+        if (typeof xmlhttpobj == und){
+            xmlhttpobj = function () {
+                try {
+                    var activex_obj = ActiveXObject;
+                } catch ( e ) {}
+                try {
+                    return new activex_obj( Msxml2+'.6.0' );
+                } catch ( e ) {}
+                try {
+                    return new activex_obj( Msxml2+'.3.0' );
+                } catch ( e ) {}
+                try {
+                    return new activex_obj( Msxml2 );
+                } catch ( e ) {}
+                throw new Error( 'This browser does not support XMLHttpRequest.' );
+            };
+        }
+        xhr = new (("onload" in new xmlhttpobj()) ? xmlhttpobj : XDomainRequest)();
 
-    try {
-        xhr.send(args.data || null);
-    } catch ( e ) {}
+        url = args.url;
 
-    if  (args.async) {
-        var timeout = setTimeout( function() {
-            xhr.abort();
-        }, args.timeout);
-    }
+        if (typeof args.data === 'string') {
+            try {args.data = JSON.parse(args.data);} catch ( e ) {}
+        }
 
-    callback = function() {
-        if ( xhr.readyState === 4  ) {
-            if  (args.async) {
-                clearTimeout(timeout);
+        if (typeof args.data === 'object') {
+            args.data = cbh.helpers.makeQueryString(args.data);
+        }
+
+        if (args.type === 'GET' && args.data && args.data.length>0) {
+            url = args.url + '?' +args.data.toString();
+        }
+        xhr.open( args.type, url, args.async );
+
+        if ( !args.crossDomain ) {
+            headers["X-Requested-With"] = "XMLHttpRequest";
+        }
+
+        for ( i in headers ) { // Support: IE<9
+            if ( headers.hasOwnProperty(i) && isset(headers[ i ]) ) {
+                xhr.setRequestHeader( i, headers[ i ] + "" );
             }
-            callback = 'undefined';
-            if ( xhr.readyState !== 4 ) {
+        }
+
+        if (args.data && args.data.toString().length>0) {
+            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            args.data = args.data.toString();
+        }
+
+        try {
+            xhr.send(args.data || null);
+        } catch ( e ) {}
+
+        if  (args.async) {
+            var timeout = setTimeout( function() {
                 xhr.abort();
-            } else {
-                status = xhr.status;
-                if ( typeof xhr.responseText === "string" ) {responses.text = xhr.responseText;}
-                try {statusText = xhr.statusText;} catch( e ) {statusText = "";}
+            }, args.timeout);
+        }
+
+        callback = function() {
+            if ( xhr.readyState === 4  ) {
+                if  (args.async) {
+                    clearTimeout(timeout);
+                }
+                callback = 'undefined';
+                if ( xhr.readyState !== 4 ) {
+                    xhr.abort();
+                } else {
+                    status = xhr.status;
+                    if ( typeof xhr.responseText === "string" ) {responses.text = xhr.responseText;}
+                    try {statusText = xhr.statusText;} catch( e ) {statusText = "";}
+                    if ( !status && !args.crossDomain ) {
+                        status = responses.text ? 200 : 404;
+                    } else if ( status === 1223 ) {
+                        status = 204;
+                    }
+                }
+            }
+
+            if ( responses ) {
                 if ( !status && !args.crossDomain ) {
                     status = responses.text ? 200 : 404;
                 } else if ( status === 1223 ) {
                     status = 204;
                 }
-            }
-        }
-
-        if ( responses ) {
-            if ( !status && !args.crossDomain ) {
-                status = responses.text ? 200 : 404;
-            } else if ( status === 1223 ) {
-                status = 204;
-            }
-            if (status === 200) {
-                if(args.dataType === 'text') {
-                    res = responses.text || '';
-                } else if (args.dataType === 'json') {
-                    res = JSON.parse(responses.text) || '';
+                if (status === 200) {
+                    if(args.dataType === 'text') {
+                        res = responses.text || '';
+                    } else if (args.dataType === 'json') {
+                        try {
+                            res = JSON.parse(responses.text) || '';
+                        } catch ( e ) {
+                            res = '';
+                        }
+                    }
+                    /**
+                     * @param a responce result
+                     * @param b responce code
+                     * @param c response text
+                     * @param d header for responce
+                     */
+                    args.complete(res, status, statusText, xhr.getAllResponseHeaders());
                 }
-                /**
-                 * @param a responce result
-                 * @param b responce code
-                 * @param c response text
-                 * @param d header for responce
-                 */
-                args.complete(res, status, statusText, xhr.getAllResponseHeaders());
             }
+        };
+
+        if ( !args.async ) {
+            callback();
+        } else if ( xhr.readyState === 4 ) { // (IE6 & IE7) if it's in cache and has been
+            setTimeout( callback, 20 );
+        } else {
+            xhr.onreadystatechange = callback;
         }
     };
-
-    if ( !args.async ) {
-        callback();
-    } else if ( xhr.readyState === 4 ) { // (IE6 & IE7) if it's in cache and has been
-        setTimeout( callback );
-    } else {
-        xhr.onreadystatechange = callback;
-    }
-};
